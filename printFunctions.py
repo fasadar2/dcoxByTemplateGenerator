22e67b03aa4b3fac884268d99db4b004c17ab62d
oma <oma@pbprog.ru>
2024-01-22 21:19:17 +0300

    добавлены мета данные, улучшен внешний вид, улучшена производительность

--- printFunctions.py
+++ printFunctions.py
@@ -1,49 +1,66 @@
+import tkinter.messagebox
+from tkinter import ttk
 import docx
 import xlrd
 from threading import Thread
 
-def CreateDocByTemplate(data: dict, templatePath: str) -> None:
-    keys = list(data.keys())
-    for i in range(len(data[keys[0]])):
+
+def CreateDocThread(data: dict, templatePath: str, coursor: int, keys: list) -> None:
+    try:
         doc = docx.Document(templatePath)
         for key in keys:
 
             for parag in doc.paragraphs:
                 if key in parag.text:
-                    parag.text = parag.text.replace(key, data[key][i])
+                    parag.text = parag.text.replace(key, data[key][coursor])
 
             for table in doc.tables:
                 for row in table.rows:
                     for cell in row.cells:
                         if key in cell.text:
-                            cell.text = cell.text.replace(key, data[key][i])
+                            cell.text = cell.text.replace(key, data[key][coursor])
+        doc.save(f"bin/{coursor}.docx")
 
-        doc.save(f"bin/{i}.docx")
+    except Exception as e:
+        tkinter.messagebox.showerror(title="Исключеньеце необработанное", message=str(e))
 
 
-def decompileExcel(dataPath:str) -> dict:
-    # Open the Workbook
-    workbook = xlrd.open_workbook(dataPath)
+def CreateDocByTemplate(data: dict, templatePath: str) -> None:
+    keys = list(data.keys())
+    try:
+        for i in range(len(data[keys[0]])):
+            Thread(target=CreateDocThread, args=(data, templatePath, i, keys)).start()
+        tkinter.messagebox.showinfo(title="Вас посетил успех", message="Файлы успешно созданы")
+    except Exception as e:
+        tkinter.messagebox.showerror(title="Исключеньеце необработанное", message=str(e))
+
+
+def DecompileExcel(dataPath: str) -> dict:
     rezult = {}
-    # Выбор листа
-    sheet = workbook.sheet_by_index(0)  # Или можно использовать sheet_by_name('имя_листа')
+    try:
+        # Open the Workbook
+        workbook = xlrd.open_workbook(dataPath)
 
-    # Получение первой строки
-    first_row = sheet.row_values(0)
+        # Выбор листа
+        sheet = workbook.sheet_by_index(0)  # Или можно использовать sheet_by_name('имя_листа')
 
-    # Формирование списка ключей
-    keys_list = [str(key) for key in first_row]
-    for key in keys_list:
-        rezult[key] = []
-    for i in range(1,sheet.nrows):
-        row = sheet.row_values(i)
-        for ii in range(len(keys_list)):
-            rezult[keys_list[ii]].append(row[ii])
+        # Получение первой строки
+        first_row = sheet.row_values(0)
 
+        # Формирование списка ключей
+        keys_list = [str(key) for key in first_row]
+        for key in keys_list:
+            rezult[key] = []
+        for i in range(1, sheet.nrows):
+            row = sheet.row_values(i)
+            for ii in range(len(keys_list)):
+                rezult[keys_list[ii]].append(row[ii])
+    except Exception as e:
+        tkinter.messagebox.showerror(title="Исключеньеце необработанное", message=str(e))
 
     return rezult
 
-def createDocx(dataPath: str, templatepath: str):
-    data = decompileExcel(dataPath)
-    t1 = Thread(target=CreateDocByTemplate, args=(data,templatepath))
-    t1.start()
+
+def CreateDocx(dataPath: str, templatePath: str):
+    data = DecompileExcel(dataPath)
+    CreateDocByTemplate(data, templatePath)
--- view.py
+++ view.py
@@ -1,55 +1,68 @@
-import tkinter  as tk
+import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-from printFunctions import createDocx
+from printFunctions import CreateDocx
+import tkinter.messagebox as message
 
 templatePath = ""
 dataPath = ""
-def readMeta():
-    meta = {"varsion":"","changes":"","author":""}
-    with open("meta.meta","r") as file:
+
+
+def ReadMeta():
+    meta = {"varsion": "", "changes": "", "author": ""}
+    with open("meta.meta", "r") as file:
         text = file.read()
-        meta["version"] = str(text[text.find("version:")+1:text.find("&&")])
-        meta["changes"] = text[text.find("changes:") + 1:text.find("&&")]
+        meta["version"] = str(text[text.find("version:") + 1:text.find("&&")])
         meta["author"] = text[text.find("author:") + 1:text.find("&&")]
     return meta
-def openTemplate():
+
+
+def OpenTemplate():
     filepath = filedialog.askopenfilename(filetypes=[("docx files", "*.docx")])
     global templatePath
     templatePath = filepath
     filepath = filepath.split("/")
-    filepath = filepath[len(filepath)-1]
+    filepath = filepath[len(filepath) - 1]
     templatePathLabel.configure(text=f"Выбранный шаблон: {filepath}")
-def openData():
-    filepath = filedialog.askopenfilename(filetypes=[("excel files",[ "*.xlsx", "*.xls"])])
+
+
+def OpenData():
+    filepath = filedialog.askopenfilename(filetypes=[("excel files", ["*.xlsx", "*.xls"])])
     global dataPath
     dataPath = filepath
     filepath = filepath.split("/")
     filepath = filepath[len(filepath) - 1]
     dataPathLabel.configure(text=f"Выбранный excel файл с данными: {filepath}")
+
+
 if __name__ == '__main__':
-    meta = readMeta()
+    meta = ReadMeta()
     mainWindow = tk.Tk()
     version = meta["version"]
+    author = meta["author"]
     mainWindow.title(f"Dcox Generator {version}")
     mainWindow.geometry("800x600")
     mainWindow.iconbitmap("mainIco.ico")
-    tk.Label(text="Instructions",font='Times 14',justify='left').pack()
-    tk.Label(text="1. Select a template",font='Times 14',justify='left').pack()
-    tk.Label(text="2. Select a data file",font='Times 14',justify='left').pack()
-    tk.Label(text="3. Press create Button",font='Times 14',justify='left').pack()
+    ttk.Label(text="Инструкция:", font='Times 14', justify='left', style="BW.TLabel")\
+        .grid(column=1, row=0,ipadx=6,columnspan=2, ipady=6, padx=4, pady=4,sticky=tk.W)
+    ttk.Label(text="1. Выберите шаблон в формате docx", font='Times 14', justify='left', style="BW.TLabel")\
+        .grid(column=1, row=1,ipadx=6,columnspan=2, ipady=6, padx=4, pady=4,sticky=tk.W)
+    ttk.Label(text="2. Выберите файл с данными в формате xls или xlsx", font='Times 14', justify='left', style="BW.TLabel")\
+        .grid(column=1, row=2,ipadx=6,columnspan=2, ipady=6, padx=4, pady=4,sticky=tk.W)
+    ttk.Label(text="3. Нажмите кнопку Заполнить документы", font='Times 14', justify='left', style="BW.TLabel")\
+        .grid(column=1, row=3,ipadx=6,columnspan=2, ipady=6, padx=4, pady=4,sticky=tk.W)
 
-    openButtonTemplate = ttk.Button(text="Выбрать шаблон",width=50, command=openTemplate)
-    openButtonTemplate.pack()
-    templatePathLabel = ttk.Label(text="Выбранный шаблон:", font='Times 12', justify='left')
-    templatePathLabel.pack()
+    openButtonTemplate = ttk.Button(text="Выбрать шаблон", width=50, command=OpenTemplate)
+    openButtonTemplate.grid(column=1, row=4,ipadx=6, ipady=6, padx=4, pady=4,sticky=tk.W)
+    templatePathLabel = ttk.Label(text="Выбранный шаблон:", font='Times 12', justify='left', style="BW.TLabel")
+    templatePathLabel.grid(column=2, row=4,ipadx=6, ipady=6, padx=4, pady=4,sticky=tk.W)
 
-    openButtonData = ttk.Button(text="Выбрать excel файл с данными",width=50, command=openData)
-    openButtonData.pack()
+    openButtonData = ttk.Button(text="Выбрать excel файл с данными", width=50, command=OpenData)
+    openButtonData.grid(column=1, row=5,ipadx=6, ipady=6, padx=4, pady=4,sticky=tk.W)
     dataPathLabel = ttk.Label(text="Выбранный excel файл с данными:", font='Times 12', justify='left')
-    dataPathLabel.pack()
-
-    CreateDocxButton = ttk.Button(text="Заполнить документ",width=50, command=lambda : createDocx(dataPath,templatePath))
-    CreateDocxButton.pack()
+    dataPathLabel.grid(column=2, row=5,ipadx=6, ipady=6, padx=4, pady=4,sticky=tk.W)
+    CreateDocxButton = ttk.Button(text="Заполнить документы", width=50,
+                                  command=lambda: CreateDocx(dataPath, templatePath))
+    CreateDocxButton.grid(column=1, row=6,ipadx=6, ipady=6, padx=4, pady=4,sticky=tk.W)
 
     mainWindow.mainloop()
